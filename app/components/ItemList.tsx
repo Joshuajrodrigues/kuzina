d8238b1b4a455d3002aaa45844f6b97d270c57c0
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-07-08 00:21:23 +0530

    icons added

--- app/components/ItemList.tsx
+++ app/components/ItemList.tsx
@@ -1,31 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
 import React from "react";
-import Card from "./Card";
 
 interface IPantryList {
-  id:string
-  ingridient:string
-  image:string
-
+  id: string;
+  ingridient: string;
+  image: string;
 }
 
-const getPantryItems = async ():Promise<IPantryList[]> => {
-  const response = await import("@/app/api/pantry/route")
-  return await ( await response.GET()).json()
+const getPantryItems = async (): Promise<IPantryList[]> => {
+  const response = await import("@/app/api/pantry/route");
+  return await (await response.GET()).json();
 };
 
 const ItemList = async () => {
   const items = await getPantryItems();
 
-  
-  return <div className="px-5 m-5">
-    {
-        items.map((item)=>(
-            <div key={item.id}>
-               <Card name={item.ingridient} src={item.image} alt={item.ingridient} />
-            </div>
-        ))
-    }
-  </div>;
+  return (
+    <div className="px-5 m-5">
+      {items.map((item) => (
+        <Card className="mb-5" key={item.id}>
+          <CardHeader>
+            <CardTitle>{item.ingridient}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Image
+              style={{
+                objectFit: "cover",
+                width:"100%"
+              }}
+              width={215}
+              height={100}
+              src={item.image}
+              alt={item.ingridient}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button>View</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default ItemList;
--- app/pantry/page.tsx
+++ app/pantry/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
-import AppliedFilters from "../components/AppliedFilters";
+import { PlusCircledIcon } from "@radix-ui/react-icons"
 import Filter from "../components/Filter";
-import { Header } from "../components/Header";
 import ItemList from "../components/ItemList";
 import Search from "../components/Search";
 import Sort from "../components/Sort";
@@ -10,16 +9,14 @@ export default function Pantry() {
   return (
     <>
     <div className=" px-5 mx-5 flex justify-between">
-      <h3 className=" text-xl">Food Pantry</h3> <Button>Add Item</Button>
+      <h3 className=" text-xl">Food Pantry</h3> <Button> <PlusCircledIcon className="mr-2 h-4 w-4"/> Add Item</Button>
     </div>
       <Search/>
       <div className=" px-5 mx-5 flex justify-between">
         <Sort/>
         <Filter/>
       </div>
-      <div  className=" px-5 m-5 flex ">
-        <AppliedFilters/>
-      </div>
+
       <section>
         <ItemList/>
       </section>
