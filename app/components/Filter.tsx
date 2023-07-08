71f4a84c1dffaa643237fcdd80d75c0ba20c706a
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-07-08 14:22:59 +0530

    filter ui

--- app/components/Filter.tsx
+++ app/components/Filter.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
+import React from "react";
 
 const Filter = () => {
   return (
-    <div>Filter</div>
-  )
-}
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button size={"sm"} className="h-8 border-dashed" variant={"outline"}>
+          <PlusCircledIcon className="mr-2 h-4 w-4" /> Filter
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent>Place content for the popover here.</PopoverContent>
+    </Popover>
+  );
+};
 
-export default Filter
+export default Filter;
