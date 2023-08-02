e3edc7b308f9be5bebcab3884112b19100384c41
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-08-02 22:40:19 +0530

    export dynamic route

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,7 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { Database } from '../../types/supabase'
 import AccountForm from './AccountForm'
-
+export const dynamic = 'force-dynamic'
 export default async function Account() {
   const supabase = createServerComponentClient<Database>({ cookies })
 
