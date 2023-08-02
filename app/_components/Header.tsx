215f8e8f0cd7d576dc4459f4c462fe27d84ccb81
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-08-02 20:19:35 +0530

    landing page kinda done

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export const Header = () => {
   return (
-    <header className=' text-3xl text-black mx-5 mb-5 p-5' >
-        KUZINA
+    <header className="mx-5 mb-5 p-5 flex justify-between">
+      <h1 className="text-3xl text-black">KUZINA</h1>
+      <section className="flex">
+        <Button className="mr-2" size={"sm"}>Sign up</Button>
+        <Button size={"sm"}>Sign in</Button>
+      </section>
     </header>
-  )
-}
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
-import { redirect } from 'next/navigation'
-
+import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   //redirect('/pantry')
   return (
-    <>
-      
-    </>
+    <div className="p-5 m-5  ">
+      <section className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl text-center">Your home kitchen management</h1>
+      </section>
+
+      <section className=" py-5 my-5 flex flex-col justify-center items-center">
+        <h2>Manage your ingridients</h2>
+        <h2>Manage your recipies</h2>
+        <h2>Collaboarate with family members</h2>
+      </section>
+
+      <section className="flex flex-col justify-center items-center">
+        <Button className="py-5 my-5">Lets get started</Button>
+      </section>
+
+      <section className=" py-5 my-5 flex flex-col ">
+        <h2 className="text-xl">How does it work ?</h2>
+        <h2 className="text-xl">Pricing</h2>
+      </section>
+    </div>
   );
 }
