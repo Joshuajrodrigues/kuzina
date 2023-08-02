bd08eb66a9e43b475d307e90a584b9a54dbf611e
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-08-02 21:48:05 +0530

    mid

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,4 +1,4 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+//import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 //import { cookies } from 'next/headers'
 //import { Database } from '../database.types'
 //import AccountForm from './AccountForm'
