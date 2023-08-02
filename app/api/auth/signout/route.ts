8e0aa989b59760248376e99f936c3d2dd9582b22
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-08-02 21:18:41 +0530

    might switch to some diffrent framework

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -1,20 +1,20 @@
 // import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 // import { cookies } from 'next/headers'
-// import { type NextRequest, NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 
-// export async function POST(req: NextRequest) {
-//   const supabase = createRouteHandlerClient({ cookies })
+export async function POST(req: NextRequest) {
+  //const supabase = createRouteHandlerClient({ cookies })
 
-//   // Check if we have a session
-//   const {
-//     data: { session },
-//   } = await supabase.auth.getSession()
+  // Check if we have a session
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession()
 
-//   if (session) {
-//     await supabase.auth.signOut()
-//   }
+  // if (session) {
+  //   await supabase.auth.signOut()
+  // }
 
-//   return NextResponse.redirect(new URL('/signin', req.url), {
-//     status: 302,
-//   })
-// }
+  return NextResponse.redirect(new URL('/signin', req.url), {
+    status: 302,
+  })
+}
