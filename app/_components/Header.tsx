022a24a7b917328201eb87fcd95a4f7175f6ab73
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-08-02 20:36:36 +0530

    wtf next js

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
   return (
     <header className="mx-5 mb-5 p-5 flex justify-between">
+      <Link href={'/'} >
       <h1 className="text-3xl text-black">KUZINA</h1>
+      </Link>
       <section className="flex">
-        <Button className="mr-2" size={"sm"}>Sign up</Button>
-        <Button size={"sm"}>Sign in</Button>
+        <Link href={'/signup'} className="mr-2 bg-primary text-sm p-2 text-white rounded-lg text-center flex items-center justify-center" >Sign up</Link>
+        <Link href={'/signin'}  className="bg-primary text-sm p-2 text-white rounded-lg text-center flex items-center justify-center"  >Sign in</Link>
       </section>
     </header>
   );
