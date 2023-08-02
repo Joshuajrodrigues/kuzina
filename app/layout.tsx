9fc7fb834b051cc4c3aa0d60ec983322abcd355f
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-08-02 23:17:54 +0530

    hero image added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Kuzine",
+  title: "Kuzina",
   description: "Your kitchen management",
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+import hero from "@/public/h2.svg";
 
 export default function Home() {
   //redirect('/pantry')
@@ -15,16 +16,45 @@ export default function Home() {
         <h2>Manage your recipies</h2>
         <h2>Collaboarate with family members</h2>
       </section>
-
+      <section className="  flex flex-col justify-center items-center">
+        <Image
+          alt="Hero"
+          src={hero}
+          //placeholder="blur"
+          quality={100}
+          width={200}
+          height={200}
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </section>
       <section className="flex flex-col justify-center items-center">
-        <Link href={'/auth'}>
-        <Button  className="py-5 my-5">Lets get started</Button>
+        <Link href={"/auth"}>
+          <Button className="py-5 my-5">Lets get started</Button>
         </Link>
       </section>
 
       <section className=" py-5 my-5 flex flex-col ">
         <h2 className="text-xl">How does it work ?</h2>
-        <h2 className="text-xl">Pricing</h2>
+        <p>
+          Kuzina is a kitchen management app designed to keep track of all your
+          kitchen inventory and your recipies. You create or join a kitchen on
+          joining in. If you make your own kitchen you can invite memebers of
+          your family to join your kitchen. Kuzina will notify you of what
+          recipies are possible to be cooked by checking your inventory. It can
+          also show you info on which items your running low on or are close to
+          expiry date. If any item is missing on a recipe it lets you assign it
+          to a shopping list. Which can be accessed by any memeber of your
+          kitchen.
+        </p>
+        <p>
+          Kuzina is designed to be incrementally adopted, so you can add items
+          to it next time you go shopping or during your free time. The best way
+          would be to start by adding your recipies and let kuzina import all
+          the inventory required from there.
+        </p>
+        {/* <h2 className="text-xl">Pricing</h2> */}
       </section>
     </div>
   );
