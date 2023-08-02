702f8707b3893e85f502be2d68846cfee3f66258
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-08-02 21:22:23 +0530

    .....

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,14 +1,14 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+//import { cookies } from 'next/headers'
 //import { Database } from '../database.types'
 //import AccountForm from './AccountForm'
 
 export default async function Account() {
-  const supabase = createServerComponentClient({ cookies })
+  //const supabase = createServerComponentClient({ cookies })
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession()
 
-  return null
+  return <></>
 }
