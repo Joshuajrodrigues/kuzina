d4eca5ede83924eda4e92ec2aa45b03f5fd7cbf7
JoshuajRodrigues <rodrigues.joshr@gmail.com>
2023-09-05 22:53:20 +0530

    invite functionality done

--- app/_components/CopyToClipboard.tsx
+++ app/_components/CopyToClipboard.tsx
@@ -1,13 +1,32 @@
+"use client"
 import { Button } from "@/components/ui/button";
-import { ClipboardCopyIcon } from "@radix-ui/react-icons";
-import React from "react";
+import { CheckIcon, ClipboardCopyIcon } from "@radix-ui/react-icons";
+import React, { useState } from "react";
 
 const CopyToClipboard = ({ id }: { id: string }) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText(id);
+    setIsCopied(true)
+    setTimeout(()=>{
+        setIsCopied(false)
+    },5000)
+  };
   return (
     <div className="flex items-center justify-start mx-5 px-5 my-0">
-      <Button className=" bg-primary-foreground text-primary text-[7px] px-2 border-white border border-dashed">
-        {" "}
-        <ClipboardCopyIcon className="mr-2" /> Copy invite code
+      <Button
+        onClick={handleCopy}
+        className=" bg-primary-foreground text-primary text-[7px] px-2 w-[100px] border-white border border-dashed"
+      >
+        {!isCopied ? (
+          <>
+            <ClipboardCopyIcon className="mr-2" /> Copy invite code
+          </>
+        ) : (
+          <>
+            <CheckIcon className="mr-2" /> Copied !
+          </>
+        )}
       </Button>
     </div>
   );
