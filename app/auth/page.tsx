fb1e3637daa2d4ee275999e0556553845c641f5f
Joshua <rodrigues.joshr@gmail.com>
2023-10-07 19:43:01 +0530

    login responsive

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import AuthForm from '../_components/AuthForm'
+import React from "react";
+import AuthForm from "../_components/AuthForm";
 
 const page = () => {
   return (
-    <div className='m-5 p-5'>
-          <h3 className=" text-xl">Authentication</h3>
-        <AuthForm />
+    <div className="m-5 p-5 md:mx-20 md:px-20 lg:mx-20 lg:px-20 xl:mx-72 xl:px-72" >
+      <h3 className=" text-xl">Authentication</h3>
+      <AuthForm />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
