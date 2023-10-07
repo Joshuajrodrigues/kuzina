832925670cce83625aa84e5a20db5d6408034a87
Joshua <rodrigues.joshr@gmail.com>
2023-10-07 20:15:54 +0530

    auth redesign

--- app/_components/AuthForm.tsx
+++ app/_components/AuthForm.tsx
@@ -12,7 +12,7 @@ export default function AuthForm() {
   return (
     <Auth
       supabaseClient={supabase}
-      view="sign_up"
+      view="sign_in"
       appearance={{ theme: ThemeSupa }}
       theme="dark"
       showLinks={false}
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,11 +1,29 @@
-import React from "react";
-import AuthForm from "../_components/AuthForm";
 
+import Image from "next/image";
+import AuthForm from "../_components/AuthForm";
+import auth from "@/public/auth.svg";
 const page = () => {
+
   return (
-    <div className="m-5 p-5 md:mx-20 md:px-20 lg:mx-20 lg:px-20 xl:mx-72 xl:px-72" >
+    <div className="m-5 p-5 my-28 md:mx-20 md:px-20 lg:mx-20 lg:px-20 xl:mx-72 xl:px-72 border border-black">
       <h3 className=" text-xl">Authentication</h3>
       <AuthForm />
+      <Image
+        alt="lady showing laptop with auth logo"
+        src={auth}
+        priority={true}
+        style={{
+          objectFit: "cover",
+        }}
+     
+        //placeholder="blur"
+        //width={100}
+       // placeholder="blur"
+       // quality={100}
+    //   width={400}
+    //    height={400}
+     
+      />
     </div>
   );
 };
